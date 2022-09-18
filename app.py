d5b1ea8f642dc3c1e0ded941b60a82a1c3514ca8
Cristiano-Rocha <sdxadev@gmail.com>
2022-09-18 12:38:35 -0300

    fix types errors

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
 from api import app, db
-from api import models
 from ariadne import (
     load_schema_from_path,
     make_executable_schema,
@@ -15,7 +14,7 @@ query = ObjectType("Query")
 query.set_field("listPosts", listPosts_resolver)
 
 type_defs = load_schema_from_path("schema.graphql")
-schema = make_executable_schema(type_defs, snake_case_fallback_resolvers)
+schema = make_executable_schema(type_defs, query, snake_case_fallback_resolvers)
 
 
 @app.route("/graphql", methods=["GET"])
